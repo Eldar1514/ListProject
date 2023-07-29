9f11ee683adc741a1a9cc7250ad52296a9784b53
Eldar199 <elikk_from_ge@mail.ru>
2023-07-30 03:54:40 +0400

    Initial commit

--- src/main/java/com/example/Test/TestController/GreetingController.java
+++ src/main/java/com/example/Test/TestController/GreetingController.java
@@ -11,9 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.naming.Binding;
+
 
 @Controller
 public class GreetingController {
